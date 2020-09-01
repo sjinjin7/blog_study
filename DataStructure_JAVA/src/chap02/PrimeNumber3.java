c823890ceffd7e3e02730ce0d560c62ded252bd7
SeoJin Kim <sjinjin7@gmail.com>
2020-09-01 22:49:01 +0900

    천 이하의 열거 3

---
+++ DataStructure_JAVA/src/chap02/PrimeNumber3.java
@@ -0,0 +1,40 @@
+package chap02;
+// 1,000 이하의 소수를 열거(버전 3)
+// 78p
+// 실습 2-11
+// 복습 ■ □ □ □ □ □ □ □ □ □ (9/1)
+
+public class PrimeNumber3 {
+
+	public static void main(String[] args) {
+		
+		int counter = 0;
+		int ptr = 0;
+		int[] prime = new int[500];
+		
+		prime[ptr++] = 2;
+		prime[ptr++] = 3;
+		
+		for(int n = 5; n <= 1000; n += 2) {
+			boolean flag = false;
+			for(int i = 1; prime[i] * prime[i] <= n; i++) {
+				counter += 2;
+				if(n % prime[i] == 0) {
+					flag = true;
+					break;
+				}
+			}// Exit for
+			if(!flag) {
+				prime[ptr++] = n;
+				counter++;
+			}
+		}// Exit for
+		
+		for(int i = 0; i < ptr; i++)
+			System.out.println(prime[i]);
+		
+		System.out.println("곱셈과 나눗셈을 수행한 횟수 : " + counter);
+		
+	}// Exit main()
+	
+}
